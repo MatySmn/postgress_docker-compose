69bb277b2fc3637ececc64ba11bd2ffc22dcfd26
MatySmn <Matys.smn@outlook.com>
2023-09-25 09:17:50 +0200

    Felder hinzugefügt

--- init.sql
+++ init.sql
@@ -1,5 +1,8 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    email TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    name TEXT NOT NULL
 );
